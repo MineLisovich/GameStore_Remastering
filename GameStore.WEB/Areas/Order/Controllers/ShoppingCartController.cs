035c02800d262cc53acf6270e04bbe49e7eca7ab
Daniil <daniil9360451@gmail.com>
2025-01-18 12:30:54 +0300

    feat: Корзина покупателя часть 1

---
+++ GameStore.WEB/Areas/Order/Controllers/ShoppingCartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.WEB.Areas.Order.Controllers
+{
+    [Area("Order")]
+    [Authorize]
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult GetShoppingCart()
+        {
+            return View();
+        }
+    }
+}
