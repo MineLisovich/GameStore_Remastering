62dc4e8d1666998956854e91c9667db03bdbd322
Daniil Zelenko <daniil9360451@gmail.com>
2025-02-06 14:15:02 +0300

    feat: страница пополнения

--- GameStore.WEB/Controllers/UserProfileController.cs
+++ GameStore.WEB/Controllers/UserProfileController.cs
@@ -122,6 +122,12 @@ namespace GameStore.WEB.Controllers
                 return Json(result);
             }
         }
+
+        [HttpGet]
+        public IActionResult AccountRefill()
+        {
+            return View();
+        }
         #endregion
 
         #region PUBLIC METHODS - POST
